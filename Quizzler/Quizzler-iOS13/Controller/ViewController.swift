a20f2d1db827c69d9bb4e76bc01e7e788da7442b
Harrison Scott <72175825+WhyNotScot@users.noreply.github.com>
2022-12-09 19:20:14 -0600

    Add files via upload

---
+++ Quizzler/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = quizBrain.getQuestionText()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnwser = sender.currentTitle
+        let userGotItRight = quizBrain.checkAnswer(userAnwser!)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        }
+        else {
+            sender.backgroundColor = UIColor.red
+        }
+                
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+
+        
+    
+    }
+    
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+        progressBar.progress = quizBrain.getProgress()
+
+    }
+    
+
+}
+
---
+++ Quizzler/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Cole Scott on 9/21/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question{
+    let text: String
+    let answer: String
+    
+    
+    init(q: String, a: String){
+        text = q
+        answer = a
+        
+        
+    }
+}
